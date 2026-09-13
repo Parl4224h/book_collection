2c7f486de6d5d00446cd8a536663ff064a8832d7
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2026-09-12 20:02:17 -0500

    Implement Book Collection 1 CRUD application (#10)
    * Implement Book Collection 1 CRUD application (#9)

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -7,35 +7,37 @@ class BooksTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit books_url
-    assert_selector "h1", text: "Books"
+    assert_selector "h1", text: "Book Collection"
   end
 
   test "should create book" do
     visit books_url
-    click_on "New book"
+    click_on "Add a Book"
 
     fill_in "Title", with: @book.title
     click_on "Create Book"
 
-    assert_text "Book was successfully created"
-    click_on "Back"
+    assert_text "Book was successfully added"
+    click_on "Home"
   end
 
   test "should update Book" do
     visit book_url(@book)
-    click_on "Edit this book", match: :first
+    click_on "Update this book", match: :first
 
     fill_in "Title", with: @book.title
     click_on "Update Book"
 
     assert_text "Book was successfully updated"
-    click_on "Back"
+    click_on "Home"
   end
 
   test "should destroy Book" do
     visit book_url(@book)
-    click_on "Destroy this book", match: :first
+    click_on "Delete this book", match: :first
+    assert_text "Are you sure you want to delete"
+    click_on "Yes, delete this book"
 
-    assert_text "Book was successfully destroyed"
+    assert_text "Book was successfully deleted"
   end
 end
